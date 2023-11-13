e11a4be997ea2d24b6f68afd45ce8ff5b202f1f8
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 14:45:41 -0800

    update tpcds data gen script

--- script/gen_tpcds_data.sh
+++ script/gen_tpcds_data.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <dump_path> <scale>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <dump_path> <scale> <rngseed>"
     exit 1
 fi
 
 DUMP_PATH=$1
 SCALE=$2
+RNGSEED=$3
 
 echo "generate data in ${1}"
-mkdir -p $DUMP_PATH && cd $DUMP_PATH
+mkdir -p $DUMP_PATH
+
+echo "data generation with scale ${2} and rngseed ${3}"
 
 dsdgen \
     -DISTRIBUTIONS $TPCHOME/tools/tpcds.idx \
     -VERBOSE Y \
+	-PARALLEL 4 \
     -FORCE \
-    -SCALE $SCALE
+	-DIR $DUMP_PATH \
+    -SCALE $SCALE \
+	-RNGSEED $RNGSEED
+
