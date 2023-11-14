3aacb5d10926f51e7f5a06e05d339fc501a02a51
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 17:44:15 -0800

    fix script

--- script/initialize_db.sh
+++ script/initialize_db.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <path-to-data-dir> <scale> <rngseed>"
-	echo "Initializes postgres db with tpcds and loads data from data directory"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <path-to-data-dir> <suffix> <scale> <rngseed>"
+	echo "Initializes postgres db with tpcds and loads data from data directory with suffix"
     exit 1
 fi
 
 DATA_DIR=$1
-SCALE=$2
-RNGSEED=$3
+SUFFIX=$2
+SCALE=$3
+RNGSEED=$4
 
 DB_NAME=tpcds-scale-${SCALE}-rngseed-${RNGSEED}
 
@@ -20,8 +21,8 @@ psql $DB_NAME -f tpcds.sql
 
 # load data
 cd $DATA_DIR
-for i in `ls *.dat`; do
-  table=${i/.dat/}
+for i in `ls *${SUFFIX}.dat`; do
+  table=${i/$SUFFIX.dat/}
   echo "Loading $table..."
   sed 's/|$//' $i > /tmp/$i
   psql $DB_NAME -q -c "TRUNCATE $table"
