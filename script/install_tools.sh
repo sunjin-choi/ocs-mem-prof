d48a18fefd11e4072b791bf2fd030a65055f6cfa
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 17:30:36 -0800

    fix install tools?

--- script/install_tools.sh
+++ script/install_tools.sh
@@ -5,7 +5,8 @@ sudo su postgres -c "createuser $(whoami) --superuser"
 
 echo "build dynamorio..."
 cd $OCSMEM_HOME
-mkdir build && cd build
+mkdir build
+cd build
 cmake --configure $DYNAMORIO_HOME
 cmake .
 make -j
