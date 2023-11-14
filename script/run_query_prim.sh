fd35a56d1db411266af1e6bae30ac605d715261c
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-14 02:02:24 -0800

    add bash for profiling prim queries

---
+++ script/run_query_prim.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Define the list of query names
+declare -a QUERY_NAMES=(
+	"cross_join" \
+	"distinct" \
+	"equijoin" \
+	"full_scan" \
+	"range_scan" \
+	"single-column_aggregation_group_by_query" \
+	"sort_order_by" \
+)
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 3 ]; then
+	echo "Usage: $0 <query_name> <scale> <rngseed>"
+	exit 1
+fi
+
+QUERY_NAME=$1
+SCALE=$2
+RNGSEED=$3
+
+# Check if QUERY_NAME is in QUERY_NAMES array
+if [[ ! " ${QUERY_NAMES[@]} " =~ " ${QUERY_NAME} " ]]; then
+	echo "Invalid query name. Available queries are: ${QUERY_NAMES[*]}"
+	exit 1
+fi
+
+## Check if the correct number of arguments are provided
+#if [ "$#" -ne 2 ]; then
+#    echo "Usage: $0 <scale> <rngseed>"
+#    exit 1
+#fi
+#
+#SCALE=$1
+#RNGSEED=$2
+
+DB_NAME=tpcds-scale-${SCALE}-rngseed-${RNGSEED}
+
+psql $DB_NAME -f $OCSMEM_HOME/tpcds-query/${QUERY_NAME}_query.sql
