9229df1b2b171d842fe1fab9b0eba241f9702061
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 22:26:52 -0800

    fix script

--- script/profile_query.sh
+++ script/profile_query.sh
@@ -10,6 +10,11 @@ QUERY_NUM=$1
 SCALE=$2
 RNGSEED=$3
 
+CSV_FILE=/tmp/tpcds-profile/query_${QUERY_NUM}_scale_${SCALE}_rngseed_${RNGSEED}.csv
+
+# setup tmp dir
+mkdir -p /tmp/tpcds-profile
+
 # run dynamorio-elam
 cd build
-./bin64/drrun -t drcachesim --simulator_type elam -- bash run_query.sh ${QUERY_NUM} ${SCALE} ${RNGSEED}
+./bin64/drrun -t drcachesim --simulator_type elam -- bash ${OCSMEM_HOME}/script/run_query.sh ${QUERY_NUM} ${SCALE} ${RNGSEED} 2> $CSV_FILE
