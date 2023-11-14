542bbd381b287e56ec78cfc0d6c2c4c7b4908824
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 21:38:06 -0800

    query scripts

---
+++ script/profile_query.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <query_num> <scale> <rngseed>"
+    exit 1
+fi
+
+QUERY_NUM=$1
+SCALE=$2
+RNGSEED=$3
+
+# run dynamorio-elam
+cd build
+./bin64/drrun -t drcachesim --simulator_type elam -- bash run_query.sh ${QUERY_NUM} ${SCALE} ${RNGSEED}
---
+++ script/run_query.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <query_num> <scale> <rngseed>"
+    exit 1
+fi
+
+QUERY_NUM=$1
+SCALE=$2
+RNGSEED=$3
+
+DB_NAME=tpcds-scale-${SCALE}-rngseed-${RNGSEED}
+
+psql $DB_NAME -f $OCSMEM_HOME/tpcds-query/query_${QUERY_NUM}.sql
+
