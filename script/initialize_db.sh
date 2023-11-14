67713484b8157274f34f32ee3ccad7dc1fcf54dc
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 17:26:51 -0800

    fix script

--- script/initialize_db.sh
+++ script/initialize_db.sh
@@ -24,6 +24,6 @@ for i in `ls *.dat`; do
   table=${i/.dat/}
   echo "Loading $table..."
   sed 's/|$//' $i > /tmp/$i
-  psql tpcds -q -c "TRUNCATE $table"
-  psql tpcds -c "\\copy $table FROM '/tmp/$i' CSV DELIMITER '|'"
+  psql $DB_NAME -q -c "TRUNCATE $table"
+  psql $DB_NAME -c "\\copy $table FROM '/tmp/$i' CSV DELIMITER '|'"
 done
