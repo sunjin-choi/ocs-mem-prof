7adde7ade79b23e6a702178d0861afa95a0e662f
Sunjin Choi <sunjin_choi@berkeley.edu>
2023-11-13 23:00:01 -0800

    add suffix so that machine type can be attached

--- script/profile_query.sh
+++ script/profile_query.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <query_num> <scale> <rngseed>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <query_num> <scale> <rngseed> <suffix>"
+	echo "suffix is usually a machine name, but any designater will work"
     exit 1
 fi
 
 QUERY_NUM=$1
 SCALE=$2
 RNGSEED=$3
+SUFFIX=$4
 
-CSV_FILE=/tmp/tpcds-trace/query_${QUERY_NUM}_scale_${SCALE}_rngseed_${RNGSEED}.csv
+CSV_FILE=/tmp/tpcds-trace/query_${QUERY_NUM}_scale_${SCALE}_rngseed_${RNGSEED}_${SUFFIX}.csv
 
 # setup tmp dir
 mkdir -p /tmp/tpcds-trace
