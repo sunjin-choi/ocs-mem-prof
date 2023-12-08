b8583fb5f8256f6333c478b0887205508f57e5c2
sunjin <gcpuser@ray-plotter32-657a-head-ff939265-compute.c.gshard-eval.internal>
2023-12-08 16:18:51 +0000

    oops

--- script/profile_query_prim.sh
+++ script/profile_query_prim.sh
@@ -39,11 +39,12 @@ if [ "$MODE" == "debug" ]; then
 	# run dynamorio-elam
 	cd build
 	echo "Running query: $QUERY_NAME"
-	./bin64/drrun -t drcachesim --simulator_type memsize -- bash ${OCSMEM_HOME}/script/run_query_prim.sh ${QUERY_NAME} ${SCALE} ${RNGSEED} ${TBL_SIZE} profile
+	#./bin64/drrun -t drcachesim --simulator_type memsize -- bash ${OCSMEM_HOME}/script/run_query_prim.sh ${QUERY_NAME} ${SCALE} ${RNGSEED} ${TBL_SIZE} profile
 else
 	# run dynamorio-elam
 	cd build
 	echo "Running query: $QUERY_NAME"
 	CSV_FILE=/tmp/db-ubmark-trace/query_${QUERY_NAME}_scale_${SCALE}_rngseed_${RNGSEED}_${QUERY_TBL}_ubmark_memsize_${TBL_SIZE}_${SUFFIX}.csv
-	./bin64/drrun -t drcachesim --simulator_type memsize -- bash ${OCSMEM_HOME}/script/run_query_prim.sh ${QUERY_NAME} ${SCALE} ${RNGSEED} ${TBL_SIZE} profile 2> $CSV_FILE
+	#./bin64/drrun -t drcachesim --simulator_type memsize -- bash ${OCSMEM_HOME}/script/run_query_prim.sh ${QUERY_NAME} ${SCALE} ${RNGSEED} ${TBL_SIZE} profile 2> $CSV_FILE
+	./bin64/drrun -t drcachesim --simulator_type elam -- bash ${OCSMEM_HOME}/script/run_query_prim.sh ${QUERY_NAME} ${SCALE} ${RNGSEED} ${TBL_SIZE} profile 2> $CSV_FILE
 fi
