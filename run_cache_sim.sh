a144499670d38184518585fa1424f74bfbb9f94c
sunjin <gcpuser@ray-test-657a-head-b0b177e5-compute.c.gshard-eval.internal>
2023-12-11 22:42:02 +0000

    ready to batch run

--- run_cache_sim.sh
+++ run_cache_sim.sh
@@ -13,6 +13,12 @@ if [ ! -f "$filename" ]; then
     echo "File not found: $filename"
     exit 1
 fi
+ 
+# Extract the base name of the file without the extension
+base_name=$(basename "$filename" | cut -d. -f1)
+
+# Output file name with 'ocs' prefix and .csv extension
+output_file="$(dirname "$filename")/ocs_${base_name}.csv"
 
 # Count the number of lines in the file
 line_count=$(wc -l < "$filename")
@@ -22,4 +28,4 @@ line_count=$(wc -l "$filename" | awk '{print $1}')
 binary="./bazel-bin/ocs_cache_sim/run_cache_sim"
 
 # Run the binary with the filename and line count as arguments
-"$binary" "$filename" -n "$line_count"
+"$binary" "$filename" -n "$line_count" -o "$output_file"
---
+++ script/sim_query.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <query_num> <scale> <rngseed> <suffix>"
+	echo "suffix is usually a machine name, but any designater will work"
+    exit 1
+fi
+
+QUERY_NUM=$1
+SCALE=$2
+RNGSEED=$3
+SUFFIX=$4
+
+CSV_FILE=/tmp/tpcds-trace/query_${QUERY_NUM}_scale_${SCALE}_rngseed_${RNGSEED}_${SUFFIX}.csv
+
+# run dynamorio-elam
+cd ${OCSMEM_HOME}
+#./bin64/drrun -t drcachesim --simulator_type elam -- bash ${OCSMEM_HOME}/script/run_query.sh ${QUERY_NUM} ${SCALE} ${RNGSEED} 2> $CSV_FILE
+./run_cache_sim.sh $CSV_FILE
